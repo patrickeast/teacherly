56e59c61dc67a9d6a0392d9296cdccb26cd24178
Patrick East <patrick.east@icloud.com>
2018-07-07 13:36:39 -0600

    Added features page for first requirement .

--- client/src/pages/Features/Features.js
+++ client/src/pages/Features/Features.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import '../../App.css';
 import Header from '../../components/Header/Header.js';
-import TeacherImg from '../../images/teacher.png';
-import PrincipalImg from '../../images/principal.png';
-import ParentsImg from '../../images/Parents.png';
+import TechImg from '../../images/Tech.png';
+import TrelloImg from '../../images/Trello.png';
+import HomeImg from '../../images/Home.jpeg';
 
 class Features extends Component {
     render() {
@@ -11,50 +11,57 @@ class Features extends Component {
             <div>
                 <Header />
                 <div className="container-fluid">
-                    <div className="row justify-content-around">
-                        <div className="col-sm-6 col-md-4 col-lg-3">
-                            <div className="card">
-                                <img className="card-img-top" src={TeacherImg} alt="Card cap" />
-                                <div className="card-body">
-                                    <h5 className="card-title">Overview</h5>
-                                    <p className="card-text">Finally, an intuitive student data tracking system that works seamlessly around your schedule.<br /><br /><b>Easy to learn. Easy to use.</b></p>
-                                    <a href="#more" className="btn btn-outline-info">Read More</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-sm-12 col-md-4 col-lg-3">
-                            <div className="card">
-                                <img className="card-img-top" src={ParentsImg} alt="Card cap" />
-                                <div className="card-body">
-                                    <h5 className="card-title">Wire Frames</h5>
-                                    <p className="card-text">Support your child's learning with a visual tool built to make your life easier.<br /><br /><b>A slick interface just for parents.</b> </p>
-                                    <a href="https://www.dropbox.com/sh/owk4qfnq9a48649/AAC8AN3Paw214CH2m2cUsP9Za?dl=0" className="btn btn-outline-info">View Wire Frames</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-sm-12 col-md-4 col-lg-3">
-                            <div className="card">
-                                <img className="card-img-top" src={PrincipalImg} alt="Card cap" />
-                                <div className="card-body">
-                                    <h5 className="card-title">Trello Board</h5>
-                                    <p className="card-text">Assess standards, view trends among classrooms and grade levels, and keep up-to-date with your teachers.<br /><br /><b>Enterprise options available.</b></p>
-                                    <a href="https://trello.com/b/RCKKnpBB/teacherly" className="btn btn-outline-info">View Trello Board</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <a name="#more"></a>
                     <div className="row">
                         <div className="col-12">
                             <div className="card">
                                 <div className="card-body">
-                                    <h5 className="card-title">Project Overview</h5>
-                                    <p className="card-text">Assess standards, view trends among classrooms and grade levels, and keep up-to-date with your teachers.<br /><br /><b>Enterprise options available.</b></p>
+                                    <h5 className="card-title">What is Teacherly?</h5>
+                                    <p className="card-text"></p>
                                 </div>
                             </div>
-
                         </div>
                     </div>
+                    <div className="row justify-content-around">
+                        <div className="col-sm-6 col-md-4 col-lg-3">
+                            <div className="card">
+                                <img className="card-img-top" src={TechImg} alt="Card cap" />
+                                <div className="card-body">
+                                    <h5 className="card-title">Tech Used</h5>
+                                    <ul className="card-text">
+                                        <li>React Router</li>
+                                        <li>Bootstrap</li>
+                                        <li>JawsDB for MySQL DB</li>
+                                        <li>Passport Local for Auth Handling</li>
+                                        <li>Customized version of ChartJS for data visualization</li>
+                                        <li>NightmareJS for testing (If I have time)</li>
+                                        <li className="disabled">More to come...</li>
+                                    </ul>
+                                    <a href="https://github.com/patrickeast/teacherly" target="_blank" className="btn btn-outline-info">View the GitHub Repo.</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm-12 col-md-4 col-lg-3">
+                            <div className="card">
+                                <img className="card-img-top" src={HomeImg} alt="Card cap" />
+                                <div className="card-body">
+                                    <h5 className="card-title">Wire Frames</h5>
+                                    <p className="card-text">I uploaded my wire frames to Dropbox. I have detailed drawings of the two main pages, with comments on each image to discuss individual features.</p>
+                                    <a href="https://www.dropbox.com/sh/owk4qfnq9a48649/AAC8AN3Paw214CH2m2cUsP9Za?dl=0" target="_blank" className="btn btn-outline-info">View Wire Frames</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm-12 col-md-4 col-lg-3">
+                            <div className="card">
+                                <img className="card-img-top" src={TrelloImg} alt="Card cap" />
+                                <div className="card-body">
+                                    <h5 className="card-title">Trello Board</h5>
+                                    <p className="card-text">Trello will be vital in keeping up-to-date with my tasks. I'm attempting to complete a lot here, even by only doing the Teacher "tier" of the product.</p>
+                                    <a href="https://trello.com/b/RCKKnpBB/teacherly" target="_blank" className="btn btn-outline-info">View Trello Board</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div >
         );
