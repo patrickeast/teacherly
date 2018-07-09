ce237e6074518fcf310ac9775562b98e29e08aed
Patrick East <patrick.east@icloud.com>
2018-07-09 13:17:15 -0600

    Teachers route fixed.

--- client/src/pages/Teachers/Teachers.js
+++ client/src/pages/Teachers/Teachers.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Header from "../../components/Header/Header.js";
 
 export default class Teachers extends Component {
     render () {
@@ -12,7 +13,6 @@ export default class Teachers extends Component {
                         </div>
                     </div>
                 </div>
-                <Footer />
             </div>
         )
     }
