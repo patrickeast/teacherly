cd70ee99430a46353588a0ce854ce689aea84deb
Patrick East <patrick.east@icloud.com>
2018-07-15 20:05:16 -0600

    Starting profile auth.

---
+++ routes/auth.js
@@ -0,0 +1,33 @@
+const authController = require('../controllers/authcontroller.js');
+
+
+module.exports = (app, passport) => {
+
+    app.get('/signup', authController.signup);
+
+    app.get('/signin', authController.signin);
+
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect: '/teachers',
+        failureRedirect: '/'
+    }
+    ));
+
+    app.get('/teachers/:id', isLoggedIn, authController.choice);
+
+    app.get('/logout', authController.logout);
+
+    app.post('/signin', passport.authenticate('local-signin', {
+        successRedirect: '/teachers',
+        failureRedirect: '/'
+    }
+    ));
+
+    function isLoggedIn(req, res, next) {
+        if (req.isAuthenticated()) {
+            return next();
+        }
+        
+        res.redirect('/signin');
+    }
+}
